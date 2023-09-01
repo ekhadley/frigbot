a49806498c4709319726b6d86048542b69a209a7
Ethan_Hadley <ekhadley@gmail.com>
2023-09-01 11:15:41 -0500

    updated arcane prediction model

--- frig.py
+++ frig.py
@@ -46,7 +46,7 @@ class Frig:
         self.loop_delay = 0.1
 
     def arcane_resp(self, msg):
-        delta = datetime.datetime(2024,11,20, 21, 5, 0) - datetime.datetime.now()
+        delta = datetime.datetime(2024,12,25, 21, 5, 0) - datetime.datetime.now()
         days, hours, minutes, seconds = delta.days, delta.seconds//3600, (delta.seconds%3600)//60, delta.seconds%60
         return f"arcane comes out in approximately {days} days, {hours} hours, {minutes} minutes, and {seconds} seconds. hang in there."
 
