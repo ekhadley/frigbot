bd12167cbc089f87c712f0b5f676ad7f88e9ade0
ekhadley <ekhadley@gmail.com>
2024-09-14 14:52:24 -0500

    new arcane date!!!

--- frigbot.py
+++ frigbot.py
@@ -86,7 +86,7 @@ class Frig:
         self.botname = self.user_IDs["FriggBot2000"]
 
     def arcane_resp(self, msg):
-        delta = datetime.datetime(2024,11,20, 21, 5, 0) - datetime.datetime.now()
+        delta = datetime.datetime(2024, 11, 8, 20, 0, 0) - datetime.datetime.now()
         years, days, hours, minutes, seconds = delta.days//365, delta.days, delta.seconds//3600, (delta.seconds%3600)//60, delta.seconds%60
         if years < 1: return f"arcane s2 comes out in approximately {days} days, {hours} hours, {minutes} minutes, and {seconds} seconds. hang in there."
         return f"arcane s2 comes out in approximately 1 year, {days-365} days, {hours} hours, {minutes} minutes, and {seconds} seconds. hang in there."
